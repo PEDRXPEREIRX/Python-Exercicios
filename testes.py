7aeeb24626b59df281502b15743f0ba450eed113
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-02 15:01:21 -0300

    condições aninhadas aula 012

--- testes.py
+++ testes.py
@@ -1,18 +1,68 @@
-from math import ceil, floor
 
-nota1 = float(input('Digite sua primeira nota: '))
-nota2 = float(input('Digite sua segunda nota: '))
-nota3 = float(input('Digite sua terceira nota: '))
 
-media = (nota1 + nota2 + nota3) / 3
 
-if (media < 7) & (media % 1 >= 0.5):
-    media = ceil(media)
 
-if media >= 7:
-    print(f'Congratulations hermano!!! Aprovado! Nota: {media:.1f}')
-else:
-    print(f'Melhore! Reprovado! Nota: {media:.1f}')
+#aula012
+#
+# nome = input('Informe seu nome: ').strip().lower()
+#
+# ESSA PARTE DAQUI EU IMPLEMENTEI COM A AJUDA DO CHATGPT PARA FAZER AS EXCECOES DOS
+# NOMES "DAS", "DOS" ... (SE CHAMA LIST COMPREHENSION) EU AINDA NAO CONHECIA MAS JA VOU
+# ANOTAR PARA ESTUDAR
+#
+# excecoes = {"das", "dos", "de", "da", "do", "e"}
+#
+# nomeFormatado = " ".join(
+#     palavra if palavra in excecoes else palavra.capitalize()
+#     for palavra in nome.split()
+# )
+#
+# ATE AQUI
+#
+# if nomeFormatado.lower() == 'pedro':
+#     print(f'{nomeFormatado}, que nome bonito :)')
+# elif nomeFormatado.lower() == 'luno':
+#     print(f'{nomeFormatado}, nome de gato safado!!!')
+# elif nomeFormatado.lower() == 'duke':
+#     print(f"{nomeFormatado}, nome de cachorro mijão '-'")
+# else:
+#     print(f'{nomeFormatado}, seja bem-vindo :)')
+#
+#fimAula012
+
+
+# nome = 'Pedro'
+# cores = {'limpa':'\033[m',
+#          'azul':'\033[1;34m',
+#          'amarelo':'\033[1;33m',
+#          'pretobranco':'\033[7;40m'}
+# print(f'Olá! Muito prazer em te conhecer, {cores['pretobranco']} {nome} {cores['limpa']}!!!')
+#
+# a = 3
+# b = 5
+# print(f'Os valores são \033[1;36m  {a}  \033[m e \033[1;31m  {b}  \033[m!')
+#
+# print('\033[1;30;47mOlá, Mundo!\033[m')
+
+
+
+
+# from math import ceil, floor
+#
+# nota1 = float(input('Digite sua primeira nota: '))
+# nota2 = float(input('Digite sua segunda nota: '))
+# nota3 = float(input('Digite sua terceira nota: '))
+#
+# media = (nota1 + nota2 + nota3) / 3
+#
+# if (media < 7) & (media % 1 >= 0.5):
+#     media = ceil(media)
+#
+# if media >= 7:
+#     print(f'Congratulations hermano!!! Aprovado! Nota: {media:.1f}')
+# else:
+#     print(f'Melhore! Reprovado! Nota: {media:.1f}')
+
 
 
 # frase = 'Curso em Vídeo Python'
