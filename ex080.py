3bea32fb142ebae9bbbe2a0402235fbf99c38e3a
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-11 19:08:57 -0300

    alteração ex080

--- ex080.py
+++ ex080.py
@@ -1,9 +1,20 @@
-from bisect import insort
-
 valores = []
 
-for _ in range(5):
+for i in range(10):
     valor = int(input('Digite um valor: '))
-    insort(valores, valor)
+    if i == 0:
+        print('Adicionado à lista!')
+        valores.append(valor)
+    elif valor > valores[-1]:
+        print('Adicionado ao final da lista!')
+        valores.append(valor)
+    else:
+        pos = 0
+        while pos < len(valores):
+            if valor <= valores[pos]:
+                print(f'Adicionado na posição {pos} da lista!')
+                valores.insert(pos, valor)
+                break
+            pos += 1
 
 print('Valores da lista em ordem:', *valores)
