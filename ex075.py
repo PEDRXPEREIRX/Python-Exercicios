697bf1419ebc20c1225f43fcc0f826bea93a124f
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-11 16:06:29 -0300

    alteração ex075

--- ex075.py
+++ ex075.py
@@ -1,24 +1,19 @@
 numeros = []
 pares = []
-tres = 0
 
 for i in range(5):
     num = int(input('Digite um número: '))
     numeros.append(num)
-    if num == 3:
-        tres += 1
     if num % 2 == 0:
         pares.append(num)
 
 tupla = tuple(numeros)
 paresTupla = tuple(pares)
 
-print(f'Você digitou os valores: ', end='')
-print(*numeros)
+print(f'Você digitou os valores: ', *numeros)
 print(f'O valor 9 apareceu {tupla.count(9)} vezes')
-if tres == 0:
-    print('O valor 3 não foi digitado em nenhuma posição')
+if 3 in numeros:
+    print(f'O valor 3 apareceu na {tupla.index(3) + 1}ª posição')
 else:
-    print(f'O valor 3 apareceu na {tupla.index(3)+1}ª posição')
-print(f'Os valores pares digitados foram: ', end='')
-print(*paresTupla)
+    print('O valor 3 não foi digitado em nenhuma posição')
+print(f'Os valores pares digitados foram: ', *paresTupla)
