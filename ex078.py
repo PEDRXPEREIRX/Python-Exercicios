3ffa8c4458a33a15c3409c00591701ca968cbed2
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-11 18:46:18 -0300

    alteração ex078

--- ex078.py
+++ ex078.py
@@ -1,8 +1,23 @@
 valores = []
+maior = menor = 0
 for i in range(5):
     valor = int(input('Digite um valor: '))
     valores.append(valor)
+    if i == 0:
+        maior = menor = valor
+    else:
+        if valor > maior:
+            maior = valor
+        if valor < menor:
+            menor = valor
 
 print('Valores da lista:', *valores)
-print(f'O maior valor da lista foi {max(valores)} na posição {valores.index(max(valores))+1}')
-print(f'O menor valor da lista foi {min(valores)} na posição {valores.index(min(valores))+1}')
+print(f'O maior valor da lista foi {maior} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == maior:
+        print(f'{i+1}... ', end='')
+print(f'\nO menor valor da lista foi {menor} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == menor:
+        print(f'{i+1}... ', end='')
+print()
