6d8e0c828ca4e8cfd24cda5408021041d59a54a6
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-15 17:46:51 -0300

    adição resolução guanabara ex084

--- ex084.py
+++ ex084.py
@@ -47,3 +47,35 @@ if maiorPesoKg == menorPesoKg:
 else:
     print(f'O maior peso foi de {maiorPesoKg:.1f}Kg. Peso de {maiorPesoNome}')
     print(f'O menor peso foi de {menorPesoKg:.1f}Kg. Peso de {menorPesoNome}')
+
+#Resolução do Curso em Vídeo - Gustavo Guanabara
+# pessoas = []
+# dados = []
+# maiorKg = menorKg = 0
+#
+# while True:
+#     dados.append(input('Nome: '))
+#     dados.append(float(input('Peso: ')))
+#     if len(pessoas) == 0:
+#         maiorKg = menorKg = dados[1]
+#     else:
+#         if dados[1] > maiorKg:
+#             maiorKg = dados[1]
+#         if dados[1] < menorKg:
+#             menorKg = dados[1]
+#     pessoas.append(dados[:])
+#     dados.clear()
+#     continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
+#     if continuar in 'N':
+#         break
+#
+# print()
+# print(f'Ao todo, foram {len(pessoas)} cadastradas.')
+# print(f'O maior peso foi de {maiorKg:.1f}Kg.', end=' ')
+# for i in pessoas:
+#     if i[1] == maiorKg:
+#         print(f'"{i[0]}"', end=' ')
+# print(f'\nO menor peso foi de {menorKg:.1f}Kg.', end=' ')
+# for i in pessoas:
+#     if i[1] == menorKg:
+#         print(f'"{i[0]}"', end=' ')
