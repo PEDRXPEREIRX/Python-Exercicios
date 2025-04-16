dca493c1c6db1c5882fd53fa00fd4a1511a4ebd5
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-16 15:59:25 -0300

    adição código do Guanabara

--- ex085.py
+++ ex085.py
@@ -18,3 +18,19 @@ valores.append(imparOrd)
 print(f'Os valores em ordem crescente de números PARES e ÍMPARES: \n{valores}\n'
       f'Somente os valores PARES: {parOrd}\n'
       f'Somente os valores ÍMPARES: {imparOrd}')
+
+#Código do Curso em Vídeo - Gustavo Guanabara
+# num = [[], []]
+#
+# for i in range(7):
+#     valor = int(input(f'Digite o {i+1}º valor: '))
+#     if valor % 2 == 0:
+#         num[0].append(valor)
+#     else:
+#         num[1].append(valor)
+#
+# num[0].sort()
+# num[1].sort()
+#
+# print(f'Os valores PARES digitados foram: {num[0]}\n'
+#       f'Os valores ÍMPARES digitados foram: {num[1]}')
