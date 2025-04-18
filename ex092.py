25ea3103a523c410395b50cee7fdfdc7d1c7d7b8
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-17 22:31:55 -0300

    alteração código ex092

--- ex092.py
+++ ex092.py
@@ -8,7 +8,7 @@ dados['ctps'] = int(input('N° carteira de trabalho: (0 não tem) '))
 if dados['ctps'] != 0:
     dados['contratação'] = int(input('Ano de contratação: '))
     dados['salario'] = float(input('Salário: R$'))
-    dados['aposentadoria'] = ((datetime.now().year - nascimento) - (datetime.now().year - dados['contratação'])) + 35
+    dados['aposentadoria'] = (dados['idade'] - (datetime.now().year - dados['contratação'])) + 35
 
 print('-=-'*10)
 print(dados)
