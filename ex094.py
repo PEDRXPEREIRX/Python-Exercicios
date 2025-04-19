a452ceb1ea9a06e1b13a95f2abcd7bb9a8f8ea1d
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-19 19:40:56 -0300

    alteração código ex094

--- ex094.py
+++ ex094.py
@@ -1,6 +1,4 @@
 pessoas = []
-mulheres = []
-mediaGrupo = []
 soma = media = 0
 while True:
     nome = input('Nome: ').strip()
@@ -8,8 +6,6 @@ while True:
     idade = int(input('Idade: ').strip())
     dados = {'nome': nome, 'sexo': sexo, 'idade': idade}
     pessoas.append(dados)
-    if sexo == 'F':
-        mulheres.append(dados['nome'])
     stop = input('Continuar cadastrando? [S/N] ').upper().strip()[0]
     if stop == 'N':
         break
@@ -18,13 +14,16 @@ print(f'Total de pessoas cadastradas: {len(pessoas)}')
 for i in range(len(pessoas)):
     print(f'[{pessoas[i]['nome']}]', end=' ')
     soma += pessoas[i]['idade']
-
 media = soma / len(pessoas)
 print()
 print(f'Média de idade do grupo: {media:.0f}')
-print(f'Mulheres do grupo:', *mulheres)
-print(f'Pessoas com idade acima da média do grupo:')
-for i in range(len(pessoas)):
-    if pessoas[i]['idade'] > media:
-        mediaGrupo.append(pessoas[i]['nome'])
-print(*mediaGrupo)
+print(f'Mulheres do grupo:', end=' ')
+for i in pessoas:
+    if i['sexo'] == 'F':
+        print(f'[{i['nome']}]', end=' ')
+print()
+print(f'Pessoas com idade acima da média do grupo:', end=' ')
+for i in pessoas:
+    if i['idade'] > media:
+        print(f'[{i['nome']}]', end=' ')
+print()
