4f99b57f135201f101d2a856edef5cd39be5ab91
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-24 12:30:40 -0300

    alteração ex103 - add gols.isdigit()

--- ex103.py
+++ ex103.py
@@ -1,12 +1,15 @@
+from unicodedata import digit
+
+
 def ficha(nome='', gols=''):
     if nome == '':
         n = '<desconhecido>'
     else:
         n = nome
-    if gols == '':
-        g = '0'
-    else:
+    if gols.isdigit():
         g = int(gols)
+    else:
+        g = 0
 
     return f'O jogador {n} fez {g} gol(s).'
 
