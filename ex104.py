e027a92948021633a7ebe8dd146efb3cd6b8dca8
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-04-24 20:18:12 -0300

    adição código Guanabara

--- ex104.py
+++ ex104.py
@@ -1,9 +1,24 @@
 def leiaInt(mensagem):
     resposta = input(mensagem)
     while not resposta.isdigit():
-        print('ERRO! Digite um número inteiro válido.')
+        print('\033[1;31mERRO! Digite um número inteiro válido.\033[m')
         resposta = input(mensagem)
     return int(resposta)
 
 n = leiaInt('Digite um número: ')
 print(f'\nVocê digitou o número {n}')
+
+#Código Guanabara
+# def leiaInt(msg):
+#     valor = 0
+#     while True:
+#         n = input(msg)
+#         if n.isnumeric():
+#             valor = int(n)
+#             break
+#         else:
+#             print('\033[1;33mERRO! Digite um número inteiro válido!\033[m')
+#     return valor
+#
+# n = leiaInt('Digite um número: ')
+# print(f'Você acabou de digitar o número {n}.')
