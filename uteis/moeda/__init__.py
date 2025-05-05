c2c6e935506c6cab8806887fc1027a385ddf5488
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-05-05 10:27:09 -0300

    realização ex109

--- uteis/moeda/__init__.py
+++ uteis/moeda/__init__.py
@@ -1,20 +1,32 @@
-def aumentar(num):
-    return num * 1.10
+def aumentar(num, cond=False):
+    if cond == True:
+        return f'R${num*1.10:.0f},00'
+    else:
+        return num * 1.10
 
 
-def diminuir(num):
-    return num - (num * 0.13)
+def diminuir(num, cond=False):
+    if cond == True:
+        return f'R${num - (num * 0.13):.0f},00'
+    else:
+        return num - (num * 0.13)
 
 
-def dobro(num):
-    return num*2
+def dobro(num, cond=False):
+    if cond == True:
+        return f'R${num*2:.0f},00'
+    else:
+        return num*2
 
 
-def metade(num):
-    return num/2
+def metade(num, cond=False):
+    if cond == True:
+        return f'R${num/2:.0f},00'
+    else:
+        return num/2
 
 
-def moeda(num):
+def moeda(num, cond=False):
     return f'R${num:.0f},00'
 
 
