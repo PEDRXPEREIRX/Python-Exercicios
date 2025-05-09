24328ffb6627d05855f5eece61ad03c4db832a9e
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-05-09 10:45:00 -0300

    aula 23 + criação arquivos ex

--- testes.py
+++ testes.py
@@ -1,9 +1,20 @@
-from uteis.utilidadesCeV import moeda, dado
-
-p = dado.leiaDinheiro('Digite o valor: R$')
-moeda.resumo(p, 35, 22)
-
-
+#aula023
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados que você digitou. ')
+except ZeroDivisionError:
+    print('Não é possível dividir por zero.')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados.')
+except Exception as erro:
+    print(f'O erro encontrado foi {erro.__cause__}')
+else:
+    print(f'O resultado é {r:.2f}')
+finally:
+    print('Volte sempre! Muito obrigado!')
 
 
 
