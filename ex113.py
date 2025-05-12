ae1d7060fed9ead9422c399a8f42f965602f9937
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-05-12 10:16:49 -0300

    realização ex113

---
+++ ex113.py
@@ -0,0 +1,21 @@
+def leiaInt(mensagem):
+    while True:
+        try:
+            resposta = int(input(mensagem))
+            return resposta
+        except ValueError:
+            print('\033[1;31mERRO: Informe um valor inteiro válido!\033[m')
+
+
+def leiaFloat(mensagem):
+    while True:
+        try:
+            resposta = float(input(mensagem))
+            return resposta
+        except ValueError:
+            print('\033[1;31mERRO: Informe um valor real válido!\033[m')
+
+
+numI = leiaInt('Digite um Inteiro: ')
+numR = leiaFloat('Digite um Real: ')
+print(f'O valor inteiro digitado foi {numI} e o valor real foi {numR}')
