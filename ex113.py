0b9fead3db6adaafa04a7da6bd504bf77024bb3b
Pedro Pereira <pedrobilharpereira@gmail.com>
2025-05-12 10:30:48 -0300

    adição código cev

--- ex113.py
+++ ex113.py
@@ -19,3 +19,36 @@ def leiaFloat(mensagem):
 numI = leiaInt('Digite um Inteiro: ')
 numR = leiaFloat('Digite um Real: ')
 print(f'O valor inteiro digitado foi {numI} e o valor real foi {numR}'.replace('.',','))
+
+#Código Guanabara:
+# def leiaInt(msg):
+#     while True:
+#         try:
+#             n = int(input(msg))
+#         except (ValueError, TypeError):
+#             print('\033[1;31mERRO: Por favor, digite um número inteiro válido!\033[m')
+#             continue
+#         except (KeyboardInterrupt):
+#             print('\n\033[1;31mEntrada de dados interrompida pelo usuário.\033[m')
+#             return 0
+#         else:
+#             return n
+#
+#
+# def leiaFloat(msg):
+#     while True:
+#         try:
+#             n = float(input(msg))
+#         except (ValueError, TypeError):
+#             print('\033[1;31mERRO: Por favor, digite um número real válido!\033[m')
+#             continue
+#         except (KeyboardInterrupt):
+#             print('\n\033[1;31mEntrada de dados interrompida pelo usuário.\033[m')
+#             return 0
+#         else:
+#             return n
+#
+#
+# n1 = leiaInt('Digite um Inteiro: ')
+# n2 = leiaFloat('Digite um Real: ')
+# print(f'O valor inteiro digitado foi {n1} e o real foi {n2} :)')
